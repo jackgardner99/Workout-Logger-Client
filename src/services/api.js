42bf7cac15f62ed380b205697e7bafa581ad4397
Jack Gardner <jtgardner99@gmail.com>
2026-05-28 23:37:07 -0500

    update url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8000'
+const BASE_URL = 'https://workout-logger-api-production.up.railway.app'
 
 async function request(path, options = {}) {
   const token = localStorage.getItem('token')
